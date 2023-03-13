71e64328ff8263e8f1a7dcf5e2b4aa212a80ab47
Ignacio <meloignacionicolas@gmail.com>
2023-03-13 19:45:21 -0300

    Clase 2 de Fod

---
+++ clase_2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*object obj=7.3;
+Console.WriteLine(obj.GetType());
+obj="casa";
+Console.WriteLine(obj);
+obj=4;
+Console.WriteLine((int)obj+1);
+object obj="cas"+5+1;
+Console.WriteLine(obj.GetType());
+Console.WriteLine(obj);
+char c1= 'a';
+char c2= 'a';
+Console.Write(c1==c2);
+object o1= 'a';
+object o2= 'a';
+Console.Write(o1==o2);
+object oo1= 'a';
+object oo2= oo1;
+Console.Write(oo1==oo2);
+*/
+int cantidad=20;
+int[] vec1= new int[cantidad];
