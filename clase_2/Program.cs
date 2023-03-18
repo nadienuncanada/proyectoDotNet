38c0b2705b841668693fd1b0b4c716fd3fe2a94c
Ignacio <meloignacionicolas@gmail.com>
2023-03-18 17:47:10 -0300

    Hasta EJER 16

--- clase_2/Program.cs
+++ clase_2/Program.cs
@@ -266,3 +266,26 @@ int Fib(int n){
 }
 Console.WriteLine(Fib(n));
 */
+/* EJERCICIO 16:
+  . Escribir una función (método int Fac(int n)) que calcule el factorial de un número n
+pasado al programa como parámetro por la línea de comando
+a) Definiendo una función no recursiva
+b) Definiendo una función recursiva
+c) idem a b) pero con expression-bodied methods (Tip: utilizar el operador condicional
+ternario)
+Console.WriteLine(FacR(5));
+int Fac(int n){
+  int m=1;
+  for(int i=1;i<=n;i++){
+    m*=i;
+  }
+ return (m);
+}
+int FacR(int n){
+ if(n<=1)
+ return 1;
+ else
+   return FacR(n-1)*n;
+}
+int FacR(int n)=> n<=1? 1:FacR(n-1)*n;
+*/
